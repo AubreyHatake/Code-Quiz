544fc7575d2997b40be90f934c3f71f27f53acc5
Aubrey Lamb <aubreyrlamb@gmail.com>
2023-03-14 20:39:28 -0600

    added psuedo code

---
+++ script.js
@@ -0,0 +1,13 @@
+// I need to create multiple buttons with event listeners that will take the user to the next question.
+// 1. start button
+// 2. x4 buttons for question 1 
+// 3. x4 buttons for question 2
+// 4. x4 buttons for question 3
+// 5. x4 buttons for question 4
+// 6. need to link highscores to the veiw highscores text on the top left of the page.
+
+
+
+
+// I need to create a box to display the user's score 
+// I need to create local storage to store all the highscores
